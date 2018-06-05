717f6683de7498ad5c54b0d5b45cdbedf45cfeb5
mbranting <mckenna.branting@gmail.com>
2018-06-05 20:30:31 +0000

    automatically backed up by learn

--- curfew_checker.rb
+++ curfew_checker.rb
@@ -1,6 +1,6 @@
 def curfew_checker (time = gets.chomp.to_i)
-  if time <=11
-    puts "pass"
+  if time <11
+    puts "you can keep having fun with the time you have left"
   elsif time >11 
     puts "fail"
   else 
